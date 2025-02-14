4b9b68a188c7c1d8b09e6162b5b96b7d5a15c8be
Vijay <vijaynatarajan@protonmail.com>
2025-02-14 16:56:16 -0500

    added code for portfolio cards

---
+++ react-portfolio/src/components/Card.jsx
@@ -0,0 +1,14 @@
+export default function Card({background, name, link}){
+    return (
+        <>
+            <section className="flex-container">
+                <section className="flex-item" style={{backgroundImage: background}}>
+                    <p className="card">{name}</p>
+                    <a href={link} target="_blank" rel="noopener noreferrer">
+                        <img src="src/assets/github-mark-white.png" alt="github" class="inside"/>
+                    </a>
+                </section>
+            </section>
+        </>
+    )
+}
--- react-portfolio/src/pages/portfolio.jsx
+++ react-portfolio/src/pages/portfolio.jsx
@@ -1,8 +1,24 @@
+import Card from "../components/Card"
 export default function Portfolio() {
     return (
         <>
             <h2>Portfolio</h2>
-            <p>placeholder</p>
+            <Card background="url(src/assets/runtracker.png)" 
+                  name="Run Tracker" 
+                  link="https://github.com/santoshalper/project1"/>
+            <Card background="url(src/assets/FiveDayWeather.png)" 
+                  name="Five Day Weather Report" 
+                  link="https://github.com/santoshalper/5dayWeatherForcast"/> 
+            <Card background="url(src/assets/ReadMeCreator.png)" 
+                  name="Read Me Creator" 
+                  link="https://github.com/santoshalper/readme-creator"/>  
+            <Card background="url(src/assets/employeeTracker.png)" 
+                  name="Employee Tracker" 
+                  link="https://github.com/santoshalper/EmployeeTracker"/>  
+            <Card background="url(src/assets/CarBuilder.png)" 
+                  name="Vehicle Builder" 
+                  link="https://github.com/santoshalper/VehicleBuilder"/>  
+
         </>
     )
 }
